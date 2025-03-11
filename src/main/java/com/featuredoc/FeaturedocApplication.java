9d1a7f12ea98980087c51518134196fe2cc2bed7
tebogomotibana <tebogo.motibana@bbd.co.za>
2025-03-11 21:14:23 +0200

    java stuff

---
+++ src/main/java/com/featuredoc/FeaturedocApplication.java
@@ -0,0 +1,13 @@
+package com.featuredoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeaturedocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeaturedocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/featuredoc/controllers/UsersControllerTest.java
@@ -0,0 +1,10 @@
+package com.featuredoc.controllers;
+import org.junit.jupiter.api.Test;
+
+public class UsersControllerTest {
+    
+    @Test
+    public void testHomeEndpoint() {
+       
+    }
+}
