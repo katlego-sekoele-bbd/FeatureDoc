e1d5322ceb5937b6920f82d7c709d57b9746c436
Keith Hughes <keith.hughes@bbd.co.za>
2025-03-18 23:11:19 +0200

    New commit to resolve conflict

--- src/main/java/com/featuredoc/FeaturedocApplication.java
+++ src/main/java/com/featuredoc/FeaturedocApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FeaturedocApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FeaturedocApplication.class, args);
 	}
 
