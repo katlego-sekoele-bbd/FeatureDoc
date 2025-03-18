69fb085baef94f71acf820f34f50b60555f35e6f
Keith Hughes <keith.hughes@bbd.co.za>
2025-03-18 22:48:38 +0200

    Added auto registration on login

--- src/main/java/com/featuredoc/models/User.java
+++ src/main/java/com/featuredoc/models/User.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 @Entity
 public class User {
 
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userID;
@@ -12,6 +14,15 @@ public class User {
     private String name;
 
     private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
 
     public Integer getUserID() {
         return userID;
