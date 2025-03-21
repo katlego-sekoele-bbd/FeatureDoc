ca1c1c3609d6895a4fa5a46ed836a0ef51912050
Cindi-Ann Jeffery <cindi-anne.jeffery@bbd.co.za>
2025-03-21 20:52:13 +0200

    auth stuff

--- src/main/java/com/featuredoc/config/SecurityConfig.java
+++ src/main/java/com/featuredoc/config/SecurityConfig.java
@@ -1,146 +1,29 @@
 package com.featuredoc.config;
 
-import com.featuredoc.filters.JwtAuthenticationFilter;
-import com.featuredoc.services.CustomOAuth2UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.featuredoc.services.UserService;
-import com.featuredoc.helpers.Jwt;
-import io.jsonwebtoken.Jwts;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import javax.crypto.SecretKey;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    public CustomOAuth2UserService customOAuth2UserService;
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    JwtAuthenticationFilter jwtAuthenticationFilter;
-
-    @Autowired
-    @Lazy
-    Jwt jwt;
-
-    @Autowired
-    @Lazy
-    private OAuth2AuthorizedClientService authorizedClientService;
-
     @Bean
-    public SecretKey secretKey() {
-        return Jwts.SIG.HS256.key().build();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests
-                        (auth -> auth
-                                .requestMatchers("/auth/token/**").permitAll()
-                                .requestMatchers(HttpMethod.GET).hasAnyRole("CAN_DELETE", "CANT_DELETE")
-                                .requestMatchers(HttpMethod.POST).hasAnyRole("CAN_DELETE", "CANT_DELETE")
-                                .requestMatchers(HttpMethod.PUT).hasAnyRole("CAN_DELETE", "CANT_DELETE")
-                                .requestMatchers(HttpMethod.PATCH).hasAnyRole("CAN_DELETE", "CANT_DELETE")
-                                .requestMatchers(HttpMethod.DELETE).hasRole("CAN_DELETE")
-                                .anyRequest().authenticated())
-                .exceptionHandling(ex -> ex
-                        .accessDeniedHandler((request, response, accessDeniedException) -> {
-                            response.setStatus(HttpStatus.UNAUTHORIZED.value());
-                            response.getWriter().write("You are not authorized to access this endpoint");
-                        }))
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Disable sessions
-                .oauth2Login(oauth2Login -> oauth2Login
-                        .userInfoEndpoint(userInfo -> userInfo
-                                .userService(oAuth2UserService()))
-                        .successHandler(successHandler())
-                        .failureHandler(failureHandler()))
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class) // Add JWT filter
-                .build();
-    }
-
-    private OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
-        return customOAuth2UserService;
-    }
-
-    private AuthenticationSuccessHandler successHandler() {
-        return (request, response, authentication) -> {
-
-            OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
-            OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(
-                    token.getAuthorizedClientRegistrationId(),
-                    token.getName());
-
-            String email = (String) token.getPrincipal().getAttributes().get("email");
-
-            String accessToken = client.getAccessToken().getTokenValue();
-
-            String jwtToken = jwt.generateJwtToken(accessToken, email, token.getAuthorities());
-
-            Map<String, String> responseData = new HashMap<>();
-            responseData.put("status", "success");
-            responseData.put("message", email + " logged in successfully");
-            responseData.put("access_token", jwtToken);
-
-            response.setStatus(HttpServletResponse.SC_ACCEPTED);
-            response.setContentType("application/json");
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            try {
-                objectMapper.writeValue(response.getWriter(), responseData);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        };
-    }
-
-    private AuthenticationFailureHandler failureHandler() {
-
-        return (request, response, authentication) -> {
-            Map<String, String> responseData = new HashMap<>();
-            responseData.put("status", "error");
-            responseData.put("message", "Login failed");
-            responseData.put("error", authentication.getMessage());
-
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-
-            response.setContentType("application/json");
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            try {
-                objectMapper.writeValue(response.getWriter(), responseData);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        };
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            return http.csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests(auth -> auth
+                            .requestMatchers("/auth/token").permitAll()
+                            .anyRequest().authenticated())
+                    .oauth2ResourceServer(oauth2 -> oauth2
+                            .jwt(jwt -> jwt
+                                    .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs") // check google public key for signature
+                            )
+                    )
+                    .build();
     }
 }
 
--- src/main/java/com/featuredoc/controllers/AuthController.java
+++ src/main/java/com/featuredoc/controllers/AuthController.java
@@ -1,49 +1,24 @@
 package com.featuredoc.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.featuredoc.models.CustomOAuth2User;
-import com.featuredoc.services.CustomOAuth2UserService;
-import io.jsonwebtoken.Jwts;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.RestTemplate;
 
-import javax.crypto.SecretKey;
-import java.io.IOException;
+import java.net.URI;
+import java.net.URLDecoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 @RestController
 public class AuthController {
-
-    @Autowired
-    SecretKey secretKey;
-
-    @Autowired
-    private CustomOAuth2UserService customOAuth2UserService;
-
-    @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
-
-    @Autowired
-    private OAuth2AuthorizedClientService authorizedClientService;
 
     @Value("${spring.security.oauth2.client.registration.google.client-id}")
     private String clientId;
@@ -52,98 +27,60 @@ public class AuthController {
     private String clientSecret;
 
     @GetMapping("/auth/token")
-    public void getToken(
-            @RequestParam("code") String code,
-            HttpServletResponse response) throws IOException {
+    public ResponseEntity<Map<String, String>> getToken(
+            @RequestParam("code") String code) throws Exception {
         String accessToken = exchangeCodeForAccessToken(code);
+
         if (accessToken == null) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            response.getWriter().write("Failed to exchange code for access token");
-            return;
+            return ResponseEntity
+                    .badRequest()
+                    .body(Collections.singletonMap("error", "Failed to exchange code for access token"));
         }
 
-        // Step 2: Load the user details using the access token
-        OAuth2User oauth2User = loadUser(accessToken);
 
-        if (oauth2User == null) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            response.getWriter().write("Failed to load user details");
-            return;
-        }
-        // Step 3: Generate a JWT token
-        String jwtToken = generateJwtToken(accessToken, oauth2User.getAttribute("email"), oauth2User.getAuthorities());
-
-        // Step 4: Return the JWT token to the client
         Map<String, String> responseData = new HashMap<>();
-        responseData.put("access_token", jwtToken);
-        responseData.put("token_type", "Bearer");
-        responseData.put("expires_in", "3600"); // Example: 1 hour
-
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.setContentType("application/json");
-        new ObjectMapper().writeValue(response.getWriter(), responseData);
+        responseData.put("access_token", accessToken);
+        return ResponseEntity.ok(responseData);
     }
 
-    private String exchangeCodeForAccessToken(String code) {
-        RestTemplate restTemplate = new RestTemplate();
+    private String exchangeCodeForAccessToken(String code) throws Exception {
+        String url = "https://oauth2.googleapis.com/token";
+        ObjectMapper objectMapper = new ObjectMapper();
+        String body =
+                "code=" + URLDecoder.decode(code, StandardCharsets.UTF_8) +
+                        "&client_id=" + clientId +
+                        "&client_secret=" + clientSecret +
+                        "&redirect_uri=" + "http://localhost:3000/login/oauth2/code/google" +
+                        "&grant_type=authorization_code" +
+                        "&scope=openid%20email%20profile";
 
-        // Prepare the request body
-        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
-        requestBody.add("code", code);
-        requestBody.add("client_id", clientId);
-        requestBody.add("client_secret", clientSecret);
-        requestBody.add("redirect_uri", "http://localhost:3000/login/oauth2/code/google");
-        requestBody.add("grant_type", "authorization_code");
+        HttpClient client = HttpClient.newHttpClient();
 
-        // Prepare the request headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .build();
 
-        // Send the request to Google's token endpoint
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(requestBody, headers);
-        ResponseEntity<Map> response = restTemplate.postForEntity(
-                "https://oauth2.googleapis.com/token",
-                request,
-                Map.class
-        );
+        HttpResponse<String> resp = client.send(request, BodyHandlers.ofString());
 
+        HashMap<String, String> map = objectMapper.readValue(resp.body(), new TypeReference<HashMap<String, String>>() {});
 
-        if (response.getStatusCode() == HttpStatus.OK && response.getBody() != null) {
-            return (String) response.getBody().get("access_token");
-        }
-
-        return null;
+        return map.get("id_token");
     }
 
-    private OAuth2User loadUser(String accessToken) {
-        // Create an OAuth2UserRequest with the access token
-        ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("google");
-        OAuth2UserRequest userRequest = new OAuth2UserRequest(clientRegistration, new OAuth2AccessToken(
-                OAuth2AccessToken.TokenType.BEARER,
-                accessToken,
-                null,
-                null
-        ));
+//    private OAuth2User loadUser(String accessToken) {
+//        // Create an OAuth2UserRequest with the access token
+//        ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("google");
+//        OAuth2UserRequest userRequest = new OAuth2UserRequest(clientRegistration, new OAuth2AccessToken(
+//                OAuth2AccessToken.TokenType.BEARER,
+//                accessToken,
+//                null,
+//                null
+//        ));
+//
+//        // Load the user details using the custom OAuth2UserService
+//        return customOAuth2UserService.loadUser(userRequest);
+//    }
 
-        // Load the user details using the custom OAuth2UserService
-        return customOAuth2UserService.loadUser(userRequest);
-    }
-
-    public String generateJwtToken(String accessToken, String email, Collection<? extends GrantedAuthority> authorities) {
-        // Create the JWT claims
-
-        List<String> roles = authorities.stream()
-                .map(GrantedAuthority::getAuthority)
-                .toList();
-
-        return Jwts.builder()
-                .claim("access_token", accessToken)
-                .claim("email", email) // Include email
-                .claim("roles", roles)// You can include the OAuth2 access token as a claim
-                .issuedAt(new Date()) // Set issue time
-                .expiration(new Date(System.currentTimeMillis() + 3600000)) // Set expiration (1 hour from now)
-                .signWith(secretKey)
-                .compact(); // Create the JWT token string
-    }
 }
